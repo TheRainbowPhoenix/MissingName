02d494c3da9f44e00f96586dc9ac778227a7e326
Pho3 <rainbowdashtheone@gmail.com>
2019-06-14 18:14:25 +0200

    fix

--- build.py
+++ build.py
@@ -22,21 +22,67 @@ OPACITY_TOLERANCE = 128
 
 # CODE
 
-if len(sys.argv)<2:
-    print("build.py c.png 200 200")
-    exit(0)
+def _make(im, width, height):
+    out = ""
 
-infile = sys.argv[1]
+    mx, my = im.size
+    if height >= my:
+        heigt = my-1
+    if width >= mx:
+        width = mx-1
+    for y in range(1, my, 2):
+        for x in range(0, mx):
+            skip = 0
 
-im = Image.open(infile)
+            layers = im.getpixel((x, y-1))
+            #print(layers)
+            if len(layers)==4:
+                r,g,b,a = layers
+            else:
+                r,g,b = layers
+                a = 255
 
-if infile.lower().endswith(".gif"):
-    im = im.convert('RGBA')
+            #r, g, b, a = im.getpixel((x, y - 1))
+            if a <= OPACITY_TOLERANCE:
+                skip += 1
+                background = "\033[49m"
+            else:
+                background = "\033[48;2;" + str(r) + ";" + str(g) + ";" + str(b) + "m"
 
-width, height = im.size
+            layers = im.getpixel((x, y))
+            if len(layers)==4:
+                r, g, b, a = layers
+            else:
+                r,g,b = layers
+                a = 255
 
-if (len(sys.argv) == 3):
-    sz = int(sys.argv[2])
+            if a <= OPACITY_TOLERANCE:
+                skip += 1
+                foreground = "\033[39m"
+            else:
+                foreground = "\033[38;2;" + str(r) + ";" + str(g) + ";" + str(b) + "m"
+
+            if skip < 2:
+                out += "{}{}▄".format(background, foreground)
+            else:
+                out += "{}{} ".format(background, foreground)
+
+        out += '\n'
+    out += "\033[m"
+    return out
+
+def _open(infile):
+    im = Image.open(infile)
+    if infile.lower().endswith(".gif"):
+        im = im.convert('RGBA')
+    
+    return im
+
+
+def makeS(infile, sz):
+    im =_open(infile)
+    width, height = im.size
+    
     sc = sz / max(width, height)
     t_width = int(width * sc)
     t_height = int(height * sc)
@@ -44,50 +90,50 @@ if (len(sys.argv) == 3):
     width = t_width
     height = t_height
 
-if (len(sys.argv) == 4):
-    t_width = min(width, int(sys.argv[2]))
-    t_height = min(height, int(sys.argv[3]))
+    return _make(im, width, height)
+   
+
+def makeXY(infile, x, y):
+    im=_open(infile)
+    width, height = im.size
+    
+    t_width = min(width, int(x))
+    t_height = min(height, int(y))
     im.thumbnail([t_width, t_height], Image.ANTIALIAS)
     width = t_width
     height = t_height
 
+    return _make(im, width, height)
+
+def make(infile):
+    im=_open(infile)
+    width, height = im.size
+
+    return _make(im, width, height)
+
+if len(sys.argv)<2:
+    print("build.py c.png 200 200")
+    exit(0)
+
+infile = sys.argv[1]
+
+
+#width, height = im.size
+
 out = ""
 
-for y in range(1, height, 2):
-    for x in range(0, width):
-        skip = 0
+if (len(sys.argv) == 3):
+    out = makeS(infile, int(sys.argv[2]))
 
-        layers = im.getpixel((x, y-1))
-        #print(layers)
-        if len(layers)==4:
-            r,g,b,a = layers
-        else:
-            r,g,b = layers
-            a = 1
+if (len(sys.argv) == 4):
+    out = makeXY(infile, sys.argv[2], sys.argv[3])
 
-
-        #r, g, b, a = im.getpixel((x, y - 1))
-        if a <= OPACITY_TOLERANCE:
-            skip += 1
-            background = "\033[49m"
-        else:
-            background = "\033[48;2;" + str(r) + ";" + str(g) + ";" + str(b) + "m"
-
-        r, g, b, a = im.getpixel((x, y))
-        if a <= OPACITY_TOLERANCE:
-            skip += 1
-            foreground = "\033[39m"
-        else:
-            foreground = "\033[38;2;" + str(r) + ";" + str(g) + ";" + str(b) + "m"
-
-        if skip < 2:
-            out += "{}{}▄".format(background, foreground)
-        else:
-            out += "{}{} ".format(background, foreground)
-
-    out += '\n'
-out += "\033[m"
+if out == "":
+    out = make(infile)
 
 f = open(infile + '.txt', 'w', encoding="utf-8")
 f.write(out)
 f.close()
+
+
+
